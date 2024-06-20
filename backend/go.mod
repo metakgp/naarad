2a110380daa59b14ecb304a0fb3de273fbd6ba48
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-20 19:20:26 +0530

    feat: added db connection

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module github.com/metakgp/naarad/backend
 
 go 1.22.4
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+)
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "github.com/joho/godotenv"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var (
+	db *sql.DB
+)
+
+func register(res http.ResponseWriter, req *http.Request) {
+
+}
 
 func main() {
 	godotenv.Load()
+
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	host := os.Getenv("DB_HOST")
+	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err = sql.Open("postgres", psqlconn)
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	http.HandleFunc("/register", register)
+	fmt.Println("Naarad Backend Server running on port : 3333")
+	err = http.ListenAndServe(":3333", nil)
+
+	if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		panic(err)
+	}
 }
