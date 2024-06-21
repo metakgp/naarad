4e6269877cffc23bfc1a7d211c1ff221013e508e
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-21 14:21:37 +0530

    feat: added making request to backend

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -4,25 +4,39 @@ import "../styles/styles.scss"
 export const Register: Component = () => {
     const [getUname, setUname] = createSignal("");
     const [getPswd, setPswd] = createSignal("");
+    const [getMsg, setMsg] = createSignal("Error in register");
+    const handleRegister = () => {
+        fetch(import.meta.env.VITE_BACKEND_URL+'/register', {
+            method: "POST",
+            body: JSON.stringify({"uname":getUname(), "pswd": getPswd()}),
+        }).then((data) => {
+            if(!data.ok){
+                data.text().then((res) => {setMsg(res)})
+            }else{
+                setMsg("Successfully Created User")
+            }
+        })
+    }
     return (
         <div class="reg">
             <div class="reg-main">
                 <div class="reg-title">
                     <div class="reg-title-name">Naarad</div>
-                    <div class="reg-title-desc">Register with a unique username and strong password</div>
+                    <div class="reg-title-desc">Register with a unique username and strong password.</div>
                 </div>
                 <div class="reg-input">
                     <div class="reg-uname">
-                        <span>Username: </span>
                         <input type="text" onChange={(e) => setUname(e.target.value)} placeholder="Enter a unique username"/>
                     </div>
                     <div class="reg-pswd">
-                        <span>Password: </span>
                         <input type="password" onChange={(e) => setPswd(e.target.value)} placeholder="Enter a secure password"/>
                     </div>
                 </div>
+                <div class="reg-msg">
+                    {getMsg()}
+                </div>
                 <div class="reg-submit">
-                    <button class="btn-reg">Register</button>
+                    <button class="btn-reg" onClick={() => handleRegister()}>Register</button>
                 </div>
             </div>
         </div>
