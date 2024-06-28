5e60e55327d453fc3ae7fc188b5369e40f0d1270
Bikram Ghuku <bikramghuku05@gmail.com>
2024-06-28 22:39:15 +0530

    feat: remove info routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,12 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 import { Route, Router } from '@solidjs/router';
 import { Register } from './pages/Register';
-import { Instruct } from './pages/Instruct';
 
 const App: Component = () => {
   return (
     <div class={styles.App}>
       <Router>
         <Route path="/" component={Register} />
-        <Route path="/help" component={Instruct} />
       </Router>
     </div>
   );
