e54502e1d31abc64b9c6bcd6ffccfec674a2c7da
proffapt <proffapt@pm.me>
2024-06-29 02:52:08 +0530

    feat: improved codebase

--- backend/main.go
+++ backend/main.go
@@ -66,80 +66,80 @@ func PasswordGenerator(passwordLength int) string {
 }
 
 func register(res http.ResponseWriter, req *http.Request) {
-	cookie, err := req.Cookie("heimdall")
-	if err != nil {
-		http.Error(res, "No JWT session token found.", http.StatusUnauthorized)
-		return
-	}
+	cookie, _ := req.Cookie("heimdall")
+	// It won't throw any error.
+	// The service will be protected by heimdall
+	// Hence if this endpoint is being triggered then
+	// It means that cookie has to be present
 	tokenString := cookie.Value
 
-	// Get email from jwt token
+	// Get email from JWT
 	reqEmail, _ := http.NewRequest("GET", "https://heimdall-api.metakgp.org/validate-jwt", nil)
 	reqEmail.Header.Set("Cookie", fmt.Sprintf("heimdall=%s", tokenString))
 	client := &http.Client{}
-
 	resp, err := client.Do(reqEmail)
 	if err != nil {
-		fmt.Println("Heimdall API Error: ", err.Error())
-		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Println("heimdall/validate-jwt Error: ", err.Error())
+		http.Error(res, "Failed to validate Heimdall session", http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
-
 	if err := json.NewDecoder(resp.Body).Decode(&jwtValidateResp); err != nil {
-		fmt.Println("Email Decoder Error: ", err.Error())
-		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Println("Heimdall Response | Email Decoder Error: ", err.Error())
+		http.Error(res, "Failed to retrieve user email", http.StatusInternalServerError)
 		return
 	}
 
-	uname := strings.TrimSuffix(jwtValidateResp.Email, "@kgpian.iitkgp.ac.in")
+	// Generate user credentials
 	userEmail := jwtValidateResp.Email
-	pswd := PasswordGenerator(pswdSize)
+	username := strings.TrimSuffix(userEmail, "@kgpian.iitkgp.ac.in")
+	password := PasswordGenerator(pswdSize)
 
 	// Create user using ntfy api
-	signupData := fmt.Sprintf(`{"username": "%s", "password": "%s"}`, uname, pswd)
+	signupData := fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password)
 	req, _ = http.NewRequest("POST", ntfyServerAddr+"/v1/account", strings.NewReader(signupData))
-
 	resp, err = client.Do(req)
 	if err != nil {
-		fmt.Println("NTFY User API error: ", err.Error())
-		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Println("NTFY User Registration API Error: ", err.Error())
+		http.Error(res, "Failed to request user registration for Naarad", http.StatusInternalServerError)
 		return
 	}
 	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		http.Error(res, "User creation Error", resp.StatusCode)
+	if resp.StatusCode == 409 {
+		fmt.Println("User already registered")
+		http.Error(res, "User already registered", resp.StatusCode)
+		return
+	} else if resp.StatusCode != 200 {
+		http.Error(res, "Failed to register user", resp.StatusCode)
 		return
 	}
 
 	// Get the userid from sqlite db
-	rowD := db.QueryRow(`SELECT id FROM user WHERE user=?`, uname)
-
+	rowD := db.QueryRow(`SELECT id FROM user WHERE user=?`, username)
 	if err = rowD.Scan(&userId); err != nil {
-		fmt.Println("Database Error: ", err.Error())
-		http.Error(res, "Internal server error", http.StatusInternalServerError)
+		fmt.Println("Database Error | Get User ID: ", err.Error())
+		http.Error(res, "Internal Server Error (DB: Fetch UserID)", http.StatusInternalServerError)
 		return
 	}
 
+	// Provide read-only access for kgp-* channels to the user
 	queryGenAccess := fmt.Sprintf(`INSERT INTO user_access VALUES("%s", "kgp-%%", 1, 0, "")`, userId)
 	if _, err = db.Exec(queryGenAccess); err != nil {
-		fmt.Println("Access generation error: ", err.Error())
-		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
+		fmt.Println("Granting Access Error: ", err.Error())
+		http.Error(res, "Internal Server Error (DB: Access Grant)", http.StatusInternalServerError)
 		return
 	}
 
-	emailBody := fmt.Sprintf("Username for signing in to Naarad portal: %s\nPassword for signing in to Naarad Portal: %s", uname, pswd)
-	sent, err := sendMail(userEmail, "Naarad Login Details | Metakgp", emailBody)
-
-	if err != nil || !sent {
-		fmt.Println("Error sending confidentials: ", err.Error())
-		http.Error(res, "Internal Server Error", http.StatusInternalServerError)
+	// Sending user credentials over mail
+	emailBody := fmt.Sprintf("Here are the credentials to sign in into Naarad.\n<b>Username</b>: %s\n<b>Password</b>: %s", username, password)
+	if sent, err := sendMail(userEmail, "Naarad Login Credentials | Metakgp", emailBody); err != nil || !sent {
+		fmt.Println("Sending Credentials Error: ", err.Error())
+		http.Error(res, "Failed to send user credentials", http.StatusInternalServerError)
 		return
 	}
 
 	http.Header.Add(res.Header(), "content-type", "application/json")
-	resStruct.Msg = "User created successfully"
+	resStruct.Msg = "User created successfully!"
 
 	if err = json.NewEncoder(res).Encode(&resStruct); err != nil {
 		http.Error(res, err.Error(), http.StatusInternalServerError)
@@ -156,7 +156,6 @@ func main() {
 	initMailer()
 
 	ntfyServerAddr = os.Getenv("NTFY_SERVER")
-
 	pswdSize, err = strconv.Atoi(os.Getenv("PASSWORD_SIZE"))
 	if err != nil {
 		pswdSize = 18
@@ -178,15 +177,12 @@ func main() {
 	}
 
 	http.HandleFunc("GET /register", register)
-
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"https://naarad.metakgp.org", "http://localhost:3000"},
 		AllowCredentials: true,
 	})
 	fmt.Println("Naarad Backend Server running on port : 5173")
-
-	err = http.ListenAndServe(":5173", c.Handler(http.DefaultServeMux))
-	if err != nil {
+	if err = http.ListenAndServe(":5173", c.Handler(http.DefaultServeMux)); err != nil {
 		fmt.Printf("error starting server: %s\n", err)
 		panic(err)
 	}
